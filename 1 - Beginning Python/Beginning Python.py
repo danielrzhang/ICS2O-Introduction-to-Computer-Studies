94d4cb0bd839c099d0d87f1f940963aaee36e2cf
Daniel Zhang <45671923+danielrzhang@users.noreply.github.com>
2021-03-22 17:25:44 -0400

    Add files via upload

---
+++ 1 - Beginning Python/Beginning Python.py
@@ -0,0 +1,23 @@
+print("Python rocks!")
+
+print("Computer Science \n @ \n RHHS")
+
+print ("The amount of tax on your new computer is ",867.53 * 0.13,"dollars.")
+
+print("There are", 232 / 60, "hours in 232 minutes.")
+
+print("The circumference of a circle with a 6 meter diameter is ", 3.141592653589793238462643383279502884197169399 * 6, "meters.")
+
+print("The average of 17, 24, and 30 is",(17 + 20 +34)/3, ".")
+
+print("PyeongChang Winter Olympic Games", 2018,)
+
+print("The answer is",(((365/12) - 28) / 2 ) * 3 + 7,".")
+
+
+
+
+
+
+
+
---
+++ 1 - Beginning Python/Variables.py
@@ -0,0 +1,23 @@
+print("Python rocks!")
+
+print("Computer Science \n @ \n RHHS")
+
+taxAmount = 867.53 * 0.13
+print("The tax amount on your new computer is $" +str(taxAmount)+ ".")
+
+hours = 232 / 60
+print("The number of hours in 232 minutes is " +str(hours)+ " hours.")
+
+circumference = 6 * 3.14
+print("The circumference of a circle with a diameter of 6 meters is " +str(circumference)+ " meters.")
+
+mean = (17 + 24 + 30) / 3
+print("The mean of 17, 24, and 30 is " +str(mean)+ ".")
+
+print("PyeongChang Winter Olympic Games", 2018,)
+
+result = (((365 / 12) - 28) / 2) * 3 + 7
+print("The result is " +str(result) + ".")
+
+
+
